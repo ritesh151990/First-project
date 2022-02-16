375386d77bfdb6e365337d6bdd078daca7620dbd
ritesh151990 <ritesh151990@gmail.com>
2022-02-16 17:59:05 +0530

    changed list

--- dom.js
+++ dom.js
@@ -1,8 +1,13 @@
 var title= document.getElementsByClassName('title');
 
 console.log(title);
-title[0].style.fontWeight='bold';
+title[0].style.fontSize='xx-large';
 title[0].style.color='green';
+var list= document.getElementsByClassName('list-group-item');
+list[2].style.color='green';
+for(let i=0;i<list.length;i++)
+    list[i].style.fontWeight='bold';
+
 
 
 
