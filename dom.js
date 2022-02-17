c68f5b27797382f38ed1105d8559059cc9c53e1b
ritesh151990 <ritesh151990@gmail.com>
2022-02-17 11:14:05 +0530

    QuerySelectorALL

--- dom.js
+++ dom.js
@@ -1,17 +1,25 @@
-var title= document.getElementsByClassName('title');
+//var title= document.getElementsByClassName('title');
 
-console.log(title);
-title[0].style.fontSize='xx-large';
-title[0].style.color='green';
-var list= document.getElementsByClassName('list-group-item');
-list[2].style.color='green';
-
-
-var li=document.getElementsByTagName('li');
-
-for(let i=0;i<li.length;i++)
-    li[i].style.fontWeight='bold';
+//console.log(title);
+//title[0].style.fontSize='xx-large';
+//title[0].style.color='green';
+//var list= document.getElementsByClassName('list-group-item');
+//list[2].style.color='green';
 
 
+//var li=document.getElementsByTagName('li');
 
+//for(let i=0;i<li.length;i++)
+   // li[i].style.fontWeight='bold';
+//var item=document.querySelector('.list-group-item');
+//item.style.color='red';
+
+var item = document.querySelectorAll('li');
+item[1].style.color='green';
+console.log(item);
+//item[1].style.backgroundColor='green';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+odd[i].style.backgroundColor='green';
+}
 
