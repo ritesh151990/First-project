c60996aa3546855714f1d803fb4ab1bde95ce4ba
ritesh151990 <ritesh151990@gmail.com>
2022-02-17 13:20:06 +0530

    add hello world'

--- dom.js
+++ dom.js
@@ -14,12 +14,45 @@
 //var item=document.querySelector('.list-group-item');
 //item.style.color='red';
 
-var item = document.querySelectorAll('li');
-item[1].style.color='green';
-console.log(item);
+//var item = document.querySelectorAll('li');
+//item[1].style.color='green';
+//console.log(item);
 //item[1].style.backgroundColor='green';
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-odd[i].style.backgroundColor='green';
-}
+//var odd=document.querySelectorAll('li:nth-child(odd)');
+//for(var i=0;i<odd.length;i++){
+//odd[i].style.backgroundColor='green';
+//}
+//var itemList=document.querySelector('#items');
+//parent node
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor='grey';
 
+
+//console.log(itemList.parentNode);
+//itemList.parentElement.style.backgroundColor='grey';
+//itemList.children[1].style.backgroundColor='yellow';
+//first element child
+//itemList.lastElementChild.textContent='Hello 4';
+var newDiv=document.createElement('div');
+newDiv.className='Hello';
+newDiv.id='hello1';
+newDiv.setAttribute('title','Hello Div');
+var newDivtext=document.createTextNode('Hello World');
+newDiv.appendChild(newDivtext);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+newDiv.style.fontsize='30px';
+container.insertBefore(newDiv,h1);
+
+var newDiv1=document.createElement('div')
+console.log(newDiv1);
+newDiv1.className='Hi';
+newDiv1.id='Hi1';
+newDiv1.setAttribute('title','hello');
+var newDiv1Text= document.createTextNode('Hello');
+newDiv1.appendChild(newDiv1Text);
+var contain=document.querySelector('#items');
+console.log(contain);
+var h2=document.querySelector('ul .list-group-item');
+console.log(h2);
+contain.insertBefore(newDiv1,h2);
